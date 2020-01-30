57a14e7ec93a0bd75a88507c5c1960a1fef91fa5
Shurara <o.rodiuk@gmail.com>
2020-01-30 16:36:54 +0200

    Update listToTree.js

--- js/listToTree.js
+++ js/listToTree.js
@@ -1,20 +1,4 @@
-let data = {
-    items: [
-        {id: 1, label: 'First item'},
-        {
-            id: 2, label: 'Second item', items: [
-                {id: 3, label: 'Sub Second item 1'},
-                {id: 4, label: 'Sub Second item 2'},
-                {id: 5, label: 'Sub Second item 3',items: [
-                        {id: 6, label: 'Third item'},
-                        {id: 7, label: 'Fourth item'}
-                    ]},
-            ]
-        },
-        {id: 8, label: 'Third item'},
-        {id: 9, label: 'Fourth item'},
-    ]
-}
+
 
 /*let data = {
     items: [
@@ -29,57 +13,77 @@ let data = {
 }*/
 
 
-let array = data.items;
-console.log(data);
-console.log(array);
+let data = {
+    items: [
+        {id: 1, label: 'First item'},
+        {
+            id: 2, label: 'Second item', items: [
+                {id: 3, label: 'Sub Second item 1'},
+                {id: 4, label: 'Sub Second item 2'},
+                {
+                    id: 5, label: 'Sub Second item 3', items: [
+                        {id: 6, label: 'Third item'},
+                        {id: 7, label: 'Fourth item'}
+                    ]
+                },
+            ]
+        },
+        {id: 8, label: 'Third item'},
+        {id: 9, label: 'Fourth item'},
+    ]
+};
 
 
-let getList = (array) => {
+function listToTree(data, options) {
+    options = options || {};
+    let ID_KEY = options.id || 'id';
+    let LABEL_KEY = options.label || 'label';
+    let ITEMS_KEY = options.items || 'items';
 
-    // array.filter(x => !x.hasOwnProperty('items')).forEach(x => createSimpleElement(x))
-
-    for (let i = 0; i < array.length; i++) {
-        let item = array[i]
-        // console.log(item.id)
-        if (!item.hasOwnProperty('items')) {
-            createSimpleElement(item)
-        } else {
-            createComplexElement(item);
+    let map = {};
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][ID_KEY]) {
+            map[data[i][ID_KEY]] = data[i];
+            data[i][ITEMS_KEY] = [];
         }
     }
-}
-
-
-getList(array);
-
-
-function createSimpleElement(item) {
-    let container = document.getElementsByClassName('container')[0];
-    let elementId = item.id;
-    let elementLabel = item.label;
-    let uList = document.createElement('ul');
-    uList.className = ('list');
-    let li = document.createElement('li');
-    li.append(document.createTextNode(elementId + " " + elementLabel));
-    uList.appendChild(li);
-    container.appendChild(uList);
-    return uList;
-}
-
-function createComplexElement(item) {
-    let base = createSimpleElement(item);
-    while (item.hasOwnProperty('items')) {
-        let elementItems = item.items;
-        let innerList = document.createElement('ul');
-        let innerLi = document.createElement('li');
-        innerLi.append(createComplexElement(elementItems));
-        innerList.append(innerLi);
-        base.appendChild(innerList);
-        return base;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][LABEL_KEY]) {
+            if (map[data[i][LABEL_KEY]]) {
+                map[data[i][LABEL_KEY]][ITEMS_KEY].push(data[i]);
+                data.splice(i, 1);
+                i--;
+            } else {
+                data[i][LABEL_KEY] = 0;
+            }
+        }
     }
-
+    return data;
 }
 
+
+let tree = listToTree(JSON.parse(JSON.stringify(data)));
+
+
+/*
+function deleteElementById(data, id) {
+    let array = data.items;
+    let newData = array.filter(x => x.id !== id);
+    return newData;
+}
+*/
+
+const removeById = (data, id) =>
+    Array.isArray(data) ? data.filter(x => x.id != id).map(x => removeById(x)) :
+        typeof(data) == 'object'
+            ? Object.entries(data).reduce((r, [k,v]) => (r[k] = removeById(v), r), {})
+            : data
+
+let deleted2 = removeById(data, 3);
+
+
+//document.getElementsByClassName("treeData")[0].innerHTML = JSON.stringify(tree, null, 2);
+document.getElementsByClassName("treeData")[0].innerHTML = JSON.stringify(deleted2, null, 2);
 
 
 
